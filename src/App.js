ad929401c06fae7b3f4aea2c898e03b93a681340
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2022-06-02 20:26:17 +0530

    style in App.css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <Router>
       <Navigation />
       <Routes>
-        <Route />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
