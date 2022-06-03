dea5db6c0c4a603d631a65f177caa2d588189cb6
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2022-06-03 18:40:58 +0530

    adding component

---
+++ src/components/Feature/Feature.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import styles from "./Feature.module.css";
+const Feature = ({ img, heading, para, link }) => {
+  return (
+    <div className={styles.feature}>
+      <div className={styles.img}>
+        <img src={`/images/${img}.svg`} alt="mic" />
+      </div>
+      <h4>{heading}</h4>
+      <p>{para}</p>
+      <Link to={`${link}`}>Learn More</Link>
+    </div>
+  );
+};
+
+export default Feature;
